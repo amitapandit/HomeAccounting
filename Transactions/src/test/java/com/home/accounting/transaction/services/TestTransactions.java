da994f7095440fe9ae22b1686a6da6ea66a38e72
PANDITAM <PANDITAM@pl1inpun0351nb.net.plm.eds.com>
2017-09-06 17:45:36 +0530

    Updated to fix all build issues

---
+++ Transactions/src/test/java/com/home/accounting/transaction/services/TestTransactions.java
@@ -0,0 +1,12 @@
+package com.home.accounting.transaction.services;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestTransactions {
+
+	@Test
+	public void testTransaction() {
+		Assert.assertNull(null);
+	}
+}
